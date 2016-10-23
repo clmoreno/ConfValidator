8f84cd284f666e3d1439077582ca603efb63fb68
Carlos Moreno <cmoreno@clmoreno.com>
2016-10-22 23:57:00 -0600

    update setup version

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from distutils.core import setup
 setup(
   name='ConfValidator',
   packages=['ConfValidator'],
+  install_requires=['configparser'],
   version='0.1',
   description='ConfValidator is a simple python3 package to easily parse INI style configuration files.',
   author='Carlos Moreno',
   author_email='cmoreno@clmoreno.com',
   url='https://github.com/clmoreno/ConfValidator',
-  download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+  download_url='https://github.com/clmoreno/ConfValidator/tarball/1.0',
   keywords=['INI', 'Configuration File', 'cfg', 'Configuration parser', 'Configuration validator'],
   classifiers=[],
 )
+
+try:
+    dfasdfa
+except Exception as e:
+    exit(e)
